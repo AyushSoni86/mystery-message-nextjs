52691af2aaa118cbc0b7190fe7bde6540254274d
AyushSoni86 <ayusoni86@gmail.com>
2025-05-07 02:39:37 +0530

    changes in env

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -13,7 +13,7 @@ const dbConnect = async (): Promise<void> => {
   }
 
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URL!);
+    const db = await mongoose.connect(process.env.MONGO_URL!);
     connection.isConnected = db.connections[0].readyState;
     console.log("DB connected successfully");
   } catch (error) {
