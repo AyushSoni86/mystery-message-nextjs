11fd359cdc587d2d43f6a454c64af8cf82ef3c03
AyushSoni86 <ayusoni86@gmail.com>
2025-05-07 02:34:10 +0530

    created models and implementated validation using zod

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+type ConnectionObect = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObect = {};
+
+const dbConnect = async (): Promise<void> => {
+  if (connection.isConnected) {
+    console.log("Already Connected to database");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URL!);
+    connection.isConnected = db.connections[0].readyState;
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.log("database connection failed", error);
+    process.exit(1);
+  }
+};
