d84dc1f1a46c90ad5b9fee99a20f21c33068aa39
AyushSoni86 <ayusoni86@gmail.com>
2025-05-08 00:01:43 +0530

    next-auth setup done

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,30 @@
+import "next-auth";
+import { DefaultSession } from "next-auth";
+
+// here we are overiding the interfaces from next-auth module
+declare module "next-auth" {
+  interface User {
+    _id?: string;
+    username: string;
+    isVerified?: boolean;
+    isAcceptingMessage: boolean;
+  }
+
+  interface Session {
+    user: {
+      _id?: string;
+      username: string;
+      isVerified?: boolean;
+      isAcceptingMessage: boolean;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    _id?: string;
+    username: string;
+    isVerified?: boolean;
+    isAcceptingMessage: boolean;
+  }
+}
